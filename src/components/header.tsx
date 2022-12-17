4b5ab33d22c9ad7062452aef727a7c25ac05de27
Darya Suravtsova <darblnkapm@gmail.com>
2022-12-18 02:17:38 +0300

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './header.module.css';
+import logo from '../images/logo-duckyduck.png';
+import basket from '../images/shopping-basket.png';
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <img src={logo} alt="Логотип" />
+          <h1>
+            Ducky<span>Duck</span>
+          </h1>
+        </div>
+        <div className={styles.basket}>
+          <div>
+            <img src={basket} alt="Корзина товаров" />
+            <p className={styles.quantity}> 0 </p>
+          </div>
+          <p className={styles.counter}> 0,00 р </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
