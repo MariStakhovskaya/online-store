69a2a665b56969da46319af1bf72d8b18f74b3d9
Darya Suravtsova <darblnkapm@gmail.com>
2022-12-18 21:26:55 +0300

    feat: add main

---
+++ src/components/Main.tsx
@@ -0,0 +1,123 @@
+import styles from './Main.module.css';
+
+function Main() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.search__and__sort}>
+        <input className={styles.search} placeholder="Поиск" />
+        <select name="sort">
+          <option> Сортировка 1 </option>
+          <option> Сортировка 2 </option>
+          <option> Сортировка 3 </option>
+        </select>
+      </div>
+      <div className={styles.products__and__filters}>
+        <div className={styles.filters}>
+          <div className={styles.category}>
+            <p> По категории </p>
+            <div className={styles.check}>
+              <Category />
+              <Category />
+              <Category />
+            </div>
+          </div>
+          <div className={styles.brand}>
+            <p> По гендеру </p>
+            <div className={styles.check}>
+              <Brand />
+              <Brand />
+            </div>
+          </div>
+          <div className={styles.price}>
+            <p> По цене </p>
+            <div className={styles.double__range}>
+              <div className={styles.values}>
+                <div className={styles.price__min}> 0 </div>
+                <div className={styles.price__max}> 100 </div>
+              </div>
+              <div className={styles.range}></div>
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value="0"
+                className={styles.range__left}
+              />
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value="100"
+                className={styles.range__right}
+              />
+            </div>
+          </div>
+          <div className={styles.ctock}>
+            <p> По наличию </p>
+            <div className={styles.double__range}>
+              <div className={styles.values}>
+                <div className={styles.price__min}> 0 </div>
+                <div className={styles.price__max}> 100 </div>
+              </div>
+              <div className={styles.range}></div>
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value="0"
+                className={styles.range__left}
+              />
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value="100"
+                className={styles.range__right}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.products}>
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Category() {
+  return (
+    <div>
+      <label>
+        <input type="checkbox" />
+        <p> Категория </p>
+      </label>
+    </div>
+  );
+}
+
+function Brand() {
+  return (
+    <div>
+      <label>
+        <input type="checkbox" />
+        <p> Гендер </p>
+      </label>
+    </div>
+  );
+}
+
+function Product() {
+  return <div className={styles.product}></div>;
+}
+
+export default Main;
