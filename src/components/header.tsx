5f70676ab522ee45e4fbbba0698b450a9566bc6f
Darya Suravtsova <darblnkapm@gmail.com>
2022-12-18 13:55:52 +0300

    feat: add footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './header.module.css';
+import styles from './Header.module.css';
 import logo from '../images/logo-duckyduck.png';
 import basket from '../images/shopping-basket.png';
 
