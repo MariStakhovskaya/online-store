04fd5fb75384b0d8604c216211a455c5315a804a
Darya Suravtsova <darblnkapm@gmail.com>
2022-12-31 13:59:18 +0300

    feat: add quantity of products and total price in header, products in basket

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -3,19 +3,24 @@ import { Button } from '../custom/button/Button';
 import style from './Product.module.css';
 import { ProductType } from '../../App';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addDuck, removeDuck } from '../../redux/slices/basketSlice';
 
 export type ProductDuckType = {
   duck: ProductType;
 };
 
 function Product({ duck }: ProductDuckType) {
+  const dispatch = useDispatch();
   const [addCart, setAddCart] = useState(false);
 
   const addToCart = () => {
     setAddCart(true);
+    dispatch(addDuck(duck));
   };
   const dropFromCart = () => {
     setAddCart(false);
+    dispatch(removeDuck(duck));
   };
   return (
     <div className={style.wrapperProduct}>
