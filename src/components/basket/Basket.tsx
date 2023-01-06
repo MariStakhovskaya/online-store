b1d533cc46d8e2efe17d6e3831ba91db36c5a7a6
Darya Suravtsova <darblnkapm@gmail.com>
2023-01-06 03:27:44 +0300

    feat: add empty basket

--- src/components/basket/Basket.tsx
+++ src/components/basket/Basket.tsx
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { ProductType } from '../../App';
 import { useState } from 'react';
+import image404 from '../../assets/img/page404.png';
 
 function Basket() {
   const { ducks, totalPrice } = useSelector((state: RootState) => state.basket);
@@ -35,56 +36,70 @@ function Basket() {
     return paginatedData;
   };
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.items}>
-        <div className={styles.total}>
-          <p className={styles.name}> Ваши товары: </p>
-          <span>
-            {ducks.reduce((sum, duck) => {
-              return duck.count + sum;
-            }, 0)}
-          </span>
-          <p>
-            (<span>{totalPrice} $</span>)
-          </p>
+  if (ducks.length) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.items}>
+          <div className={styles.total}>
+            <p className={styles.name}> Ваши товары: </p>
+            <span>
+              {ducks.reduce((sum, duck) => {
+                return duck.count + sum;
+              }, 0)}
+            </span>
+            <p>
+              (<span>{totalPrice} $</span>)
+            </p>
+          </div>
+          <div className={styles.quantity__items}>
+            <p> Товаров на странице: </p>
+            <input
+              type="number"
+              onChange={(e) => {
+                changeValue(parseInt(e.target.value));
+              }}
+              min="1"
+              max={ducks.length}
+              defaultValue={ducks.length}
+              className={styles.quantity}
+            />
+          </div>
         </div>
-        <div className={styles.quantity__items}>
-          <p> Товаров на странице: </p>
-          <input
-            type="number"
-            onChange={(e) => {
-              changeValue(parseInt(e.target.value));
-            }}
-            min="1"
-            max={ducks.length}
-            defaultValue={ducks.length}
-            className={styles.quantity}
-          />
+        <div className={styles.products}>
+          {ducks &&
+            displayList(ducks, quantity, page).map((duck) => {
+              return <ProductBasket key={duck.id} duck={duck} />;
+            })}
+        </div>
+        <div className={styles.btns}>
+          <div>
+            <button className={styles.buy}> КУПИТЬ </button>
+            <p className={styles.promo__text}> ПРОМОКОД: </p>
+            <input type="text" value="promoUtka" className={styles.promo} />
+          </div>
+          <div className={styles.navigation}>
+            <Button name={'<'} callback={lessPage} />
+            <p className={styles.page}>
+              {page + 1} / {Math.ceil(ducks.length / quantity)}
+            </p>
+            <Button name={'>'} callback={morePage} />
+          </div>
         </div>
       </div>
-      <div className={styles.products}>
-        {ducks &&
-          displayList(ducks, quantity, page).map((duck) => {
-            return <ProductBasket key={duck.id} duck={duck} />;
-          })}
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <p className={styles.empty}> Корзина пуста! </p>
+        <img
+          src={image404}
+          height="250px"
+          alt="Уточка"
+          className={styles.duck}
+        />
       </div>
-      <div className={styles.btns}>
-        <div>
-          <button className={styles.buy}> КУПИТЬ </button>
-          <p className={styles.promo__text}> ПРОМОКОД: </p>
-          <input type="text" value="promoUtka" className={styles.promo} />
-        </div>
-        <div className={styles.navigation}>
-          <Button name={'<'} callback={lessPage} />
-          <p className={styles.page}>
-            {page + 1} / {Math.ceil(ducks.length / quantity)}
-          </p>
-          <Button name={'>'} callback={morePage} />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Basket;
